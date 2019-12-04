4ec93dee104d65ff50c86a91bde9a9dd61ed701f
EPQSky <1170940270@qq.com>
2019-12-04 10:22:26 +0800

    database

--- src/main/java/cn/net/epq/community/contorller/AuthorizeContorller.java
+++ src/main/java/cn/net/epq/community/contorller/AuthorizeContorller.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeContorller {
 
@@ -24,7 +26,8 @@ public class AuthorizeContorller {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -33,7 +36,13 @@ public class AuthorizeContorller {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null){
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/"; //跳转到index页面
+        }else {
+            //登陆失败,重新登陆
+            return "redirect:/"; //跳转到index页面
+        }
     }
 }
