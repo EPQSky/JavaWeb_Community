b21160351db2f0c2deff190d3c4c10728b20dc67
EPQSky <1170940270@qq.com>
2019-12-07 22:28:55 +0800

    update

--- src/main/java/cn/net/epq/community/contorller/IndexContorller.java
+++ src/main/java/cn/net/epq/community/contorller/IndexContorller.java
@@ -1,13 +1,19 @@
 package cn.net.epq.community.contorller;
 
+import cn.net.epq.community.dto.QuestionDTO;
+import cn.net.epq.community.mapper.QuestionMapper;
 import cn.net.epq.community.mapper.UserMapper;
+import cn.net.epq.community.model.Question;
 import cn.net.epq.community.model.User;
+import cn.net.epq.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexContorller {
@@ -15,8 +21,12 @@ public class IndexContorller {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model){
 
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
@@ -30,7 +40,8 @@ public class IndexContorller {
                     break;
                 }
             }
-
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
---
+++ src/main/java/cn/net/epq/community/service/QuestionService.java
@@ -0,0 +1,36 @@
+package cn.net.epq.community.service;
+
+import cn.net.epq.community.dto.QuestionDTO;
+import cn.net.epq.community.mapper.QuestionMapper;
+import cn.net.epq.community.mapper.UserMapper;
+import cn.net.epq.community.model.Question;
+import cn.net.epq.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<QuestionDTO> list() {
+        List<Question> questions = questionMapper.list();
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions){
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        return questionDTOList;
+    }
+}
